3bcd33fe0d5347b7530b329dea04e8a8e3da3af4
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-09-19 20:43:28 +0300

    codewars-you-only-need-one-beginner

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,27 @@
+//You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+//Array can contain numbers or strings. X can be either.
+// Return true if the array contains the value, false if not.
+
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MyTest {
     @Test
-    void name() {
+        public void testSomething() {
+            assertEquals(Solution.check(new Object[]{66, 101}, 66), true);
+            assertEquals(Solution.check(new Object[]{80, 117, 115, 104, 45, 85, 112, 115}, 45), true);
+            assertEquals(Solution.check(new Object[]{'t', 'e', 's', 't'}, 'e'), true);
+            assertEquals(Solution.check(new Object[]{"what", "a", "great", "kata"}, "kat"), false);
+        }
 
     }
-}
+
+    class Solution {
+        public static boolean check(Object[] a, Object x) {
+            return Arrays.asList(a).contains(x);
+        }
+    }
+
